b4af5fcaeac0ecfce6ba15b300a6192b00076642
Renan Matias <rnmatias@hotmail.com>
2022-02-20 16:03:56 -0300

    45. Inserted data in view Home list

---
+++ utils/recipes/factory.py
@@ -0,0 +1,34 @@
+# from inspect import dignature
+from random import randint
+from faker import Faker
+
+
+def rand_radio():
+    return randint(840, 900), randint(473, 573)
+
+
+fake = Faker('pt-BR')
+# print(signature(fake.random_number))
+
+
+def make_recipe():
+    return {
+        'title': fake.sentence(nb_words=6),
+        'description': fake.sentence(nb_words=12),
+        'preparation_time': fake.random_number(digits=2, fix_len=True),
+        'preparation_time_unit': 'Minutos',
+        'serving': fake.random_number(digits=2, fix_len=True),
+        'serving_unit': 'Porções',
+        'preparation_steps': fake.text(3000),
+        'created_at': fake.date_time(),
+        'author': {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+        },
+        'category': {
+            'name': fake.word()
+        },
+        'cover': {
+            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_radio(),
+        }
+    }
