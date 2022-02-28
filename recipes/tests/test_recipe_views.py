1dcc26c84616ae1371ba2e05875b7d6d965f6f61
Renan Matias <rnmatias@hotmail.com>
2022-02-28 17:38:19 -0300

    81. Tested response context and content in the template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -50,7 +50,13 @@ class RecipeViewsTest(TestCase):
             preparation_step_is_html=False,
             is_published=True,
         )
-        ...
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     # CATEGORY
 
