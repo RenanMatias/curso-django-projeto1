badb4c7d2ce1ed20639072c2b006321f43478f15
Renan Matias <rnmatias@hotmail.com>
2022-03-01 10:59:23 -0300

    95. Tested category model

--- recipes/tests/test_recipe_model_category.py
+++ recipes/tests/test_recipe_model_category.py
@@ -1,6 +1,24 @@
+from django.core.exceptions import ValidationError
 from .test_recipe_base import RecipeTestBase
 
 
-class RecipeModelTest(RecipeTestBase):
-    def test_recipe_string_representation(self):
-        ...
+class RecipeCategoryModelTest(RecipeTestBase):
+
+    def setUp(self) -> None:
+        self.category = self.make_category(
+            name='Category Testing'
+        )
+        return super().setUp()
+
+    def test_recipe_category_model_string_representation_is_name_field(self):
+        self.assertEqual(
+            str(self.category),
+            self.category.name,
+            msg=f'Category string representation must be "{self.category.name}" '
+                f'but "{str(self.category)}" was received.'
+        )
+
+    def test_recipe_category_model_name_max_lenght_is_65_chars(self):
+        self.category.name = 'A' * 66
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
