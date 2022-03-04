217c8859fca71b26f591fd354085e23e3c13e30f
Renan Matias <rnmatias@hotmail.com>
2022-03-04 08:59:00 -0300

    99. Created a new search URL with TDD (Test Driven Development)

--- recipes/urls.py
+++ recipes/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
         views.category, name='category'
     ),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
+    path('recipes/search/', lambda request: ..., name='search'),
 ]
