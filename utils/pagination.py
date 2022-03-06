6b3cfef9b953189bb8f419bcae4d1b08b4355327
Renan Matias <rnmatias@hotmail.com>
2022-03-06 20:58:08 -0300

    118. Added pagination in template

--- utils/pagination.py
+++ utils/pagination.py
@@ -23,7 +23,7 @@ def make_pagination_range(page_range, qty_pages, current_page):
         'page_range': page_range,
         'qty_pages': qty_pages,
         'current_page': current_page,
-        'total:pages': total_pages,
+        'total_pages': total_pages,
         'start_range': start_range,
         'stop_range': stop_range,
         'first_page_out_of_range': current_page > middle_range,
