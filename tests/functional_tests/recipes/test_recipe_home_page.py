c784d87ce19893ef710f915ea4981514a636ef64
Renan Matias <rnmatias@hotmail.com>
2022-03-29 09:45:05 -0300

    170. Found inputs and typing texts with selenium (Keys)

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from .base import RecipeBaseFunctionalTest
 
@@ -13,3 +14,31 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Nenhuma receita encontrada 😔', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # User opens the page
+        self.browser.get(self.live_server_url)
+
+        # User sees a search field with the text "Search for a recipe"
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+
+        # Click on this input and type the search term to find the recipe with the desired title
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        # User sees what they were looking for
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
