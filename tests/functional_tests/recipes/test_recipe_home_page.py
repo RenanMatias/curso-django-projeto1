ca76feeac6c552eca0678c5548b30fc5fe70854d
Renan Matias <rnmatias@hotmail.com>
2022-03-29 20:14:07 -0300

    171. Functional Test to pagination with accessibility (ARIA)

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -42,3 +42,24 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
             title_needed,
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
         )
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # User opens the page
+        self.browser.get(self.live_server_url)
+
+        # User sees that it have a pagination and click on page 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+
+        page2.click()
+
+        # User sees there are more 2 recipes on page 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
+        )
