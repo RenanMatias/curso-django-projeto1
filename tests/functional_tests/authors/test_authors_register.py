fe15df4e474c0365503d118bba8248df0aa17931
Renan Matias <rnmatias@hotmail.com>
2022-03-29 20:26:59 -0300

    172. Created the base for authors tests

---
+++ tests/functional_tests/authors/test_authors_register.py
@@ -0,0 +1,11 @@
+import pytest
+
+from .base import AuthorsBaseTest
+
+
+@pytest.mark.functional_test
+class AuthorsRegisterTest(AuthorsBaseTest):
+    def test_the_test(self):
+        self.browser.get(self.live_server_url + '/authors/register/')
+
+        self.sleep()
