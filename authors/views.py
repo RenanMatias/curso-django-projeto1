a556e81a91292124bc14d24769e4c4766f8dc091
Renan Matias <rnmatias@hotmail.com>
2022-04-08 11:35:29 -0300

    188. Saved the reciped editions on database and request.FILES to Cover

--- authors/views.py
+++ authors/views.py
@@ -114,8 +114,23 @@ def dashboard_recipe_edit(request, id):
 
     form = AuthorRecipeForm(
         data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe,
     )
+
+    if form.is_valid():
+        # Now, the form is valid and we can try to save it
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Your recipe has been saved successfully!')
+
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
 
     return render(
         request,
