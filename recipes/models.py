049c7c1c7b52a13a38beb15906d3b6a1fd52b6bb
Renan Matias <rnmatias@hotmail.com>
2022-04-10 21:07:58 -0300

    196. Added get_absolute_url in model

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.urls import reverse
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
@@ -34,3 +36,6 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('recipes:recipe', args=(self.id,))
