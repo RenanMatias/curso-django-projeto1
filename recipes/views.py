82473ac0d86f08579ebd994f37eebe0a4210f772
Renan Matias <rnmatias@hotmail.com>
2022-04-12 16:05:32 -0300

    207. Added Search ListView

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,7 @@
 import os
 
 from django.db.models import Q
-from django.http import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from utils.pagination import make_pagination
 
@@ -61,6 +60,29 @@ class RecipeListViewCategory(RecipeListViewBase):
 class RecipeListViewSearch(RecipeListViewBase):
     template_name = 'recipes/pages/search.html'
 
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '').strip()
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term)
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '').strip()
+
+        ctx.update({
+            'page_title': f'Pesquisa por "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}'
+        })
+
+        return ctx
+
 
 def recipe(request, id):
 
@@ -73,27 +95,4 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True
-    })
-
-
-def search(request):
-    search_term = request.GET.get('q', '').strip()
-
-    if not search_term:
-        raise Http404()
-
-    recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) |
-        Q(description__icontains=search_term),
-        Q(is_published=True),
-    ).order_by('-id')
-
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    return render(request, 'recipes/pages/search.html', {
-        'page_title': f'Pesquisa por "{search_term}" |',
-        'search_term': search_term,
-        'recipes': page_obj,
-        'pagination_range': pagination_range,
-        'additional_url_query': f'&q={search_term}'
     })
