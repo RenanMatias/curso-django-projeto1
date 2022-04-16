b0bc574332e94b8579c0ae4ef751e7d47525065a
Renan Matias <rnmatias@hotmail.com>
2022-04-16 19:48:30 -0300

    231. Avoided unnecessary queries

--- recipes/views.py
+++ recipes/views.py
@@ -133,6 +133,7 @@ class RecipeDetail(DetailView):
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(is_published=True,)
+        qs = qs.select_related('author', 'category')
         return qs
 
 
