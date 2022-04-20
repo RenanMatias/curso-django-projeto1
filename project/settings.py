ccfb66e84a99d7734815936ad33390d46638b301
Renan Matias <rnmatias@hotmail.com>
2022-04-20 18:50:19 -0300

    Add load_dotenv on settings.py

--- project/settings.py
+++ project/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 import os
 from pathlib import Path
 
 from django.contrib.messages import constants
+
+if os.environ.get('DEBUG', None) is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
