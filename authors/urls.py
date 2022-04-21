a0598db43481a38ba9e15c743eb47ffdfe2152e4
Renan Matias <rnmatias@hotmail.com>
2022-04-21 11:03:47 -0300

    256. Created Logic and HTML content with the menu structure

--- authors/urls.py
+++ authors/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('login/create/', views.login_create, name='login_create'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/', views.dashboard, name='dashboard'),
     path(
         'dashboard/recipe/new/',
         views.DashboardRecipe.as_view(),
