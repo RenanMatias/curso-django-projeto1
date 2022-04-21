d502bad1c91035636999811da62f30100b47f1df
Renan Matias <rnmatias@hotmail.com>
2022-04-20 21:49:41 -0300

    255. Data validation in the model itself (single point of validation)

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import F, Value
 from django.db.models.functions import Concat
+from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 from tag.models import Tag
@@ -72,3 +75,19 @@ class Recipe(models.Model):
             self.slug = slug
 
         return super().save(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        error_message = defaultdict(list)
+
+        recipe_from_db = Recipe.objects.filter(
+            title__iexact=self.title
+        ).first()
+
+        if recipe_from_db:
+            if recipe_from_db.pk != self.pk:
+                error_message['title'].append(
+                    'Recipe with this title already exists.'
+                )
+
+        if error_message:
+            raise ValidationError(error_message)
