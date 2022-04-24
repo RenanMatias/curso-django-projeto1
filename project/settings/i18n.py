37c480d589d821c0a1b13cc8ff8da67880e6cc10
Renan Matias <rnmatias@hotmail.com>
2022-04-24 08:57:08 -0300

    267. Config USE_I18N, LOCALE_PATH and MIDDLEWARE

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,5 +1,6 @@
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
+from . import BASE_DIR
 
 LANGUAGE_CODE = 'pt-BR'
 
@@ -8,3 +9,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATH = [
+    BASE_DIR / 'locale',
+]
