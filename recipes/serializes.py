096e773f01181cb4d02a2ef072a48dd9e4223de1
Renan Matias <rnmatias@hotmail.com>
2022-04-25 18:35:35 -0300

    283. Used a serializer for list and detail with Django Rest Framework

--- recipes/serializes.py
+++ recipes/serializes.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_lenght=65)
-    description = serializers.CharField(max_lenght=165)
+    title = serializers.CharField(max_length=65)
+    description = serializers.CharField(max_length=165)
