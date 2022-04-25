4818613589618b01876617fc8b64887423c1f667
Renan Matias <rnmatias@hotmail.com>
2022-04-25 20:38:11 -0300

    285. Related Fields in Django Rest Framework

--- recipes/serializes.py
+++ recipes/serializes.py
@@ -1,6 +1,14 @@
 # Reference: https://www.django-rest-framework.org/api-guide/fields/
 
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -9,6 +17,18 @@ class RecipeSerializer(serializers.Serializer):
     description = serializers.CharField(max_length=165)
     public = serializers.BooleanField(source='is_published')
     preparation = serializers.SerializerMethodField()
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True
+    )
+    tags_objects = TagSerializer(
+        many=True,
+        source='tags'
+    )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
