1a925f18b58da6ddecbc401c67ec92e96720cd9b
Renan Matias <rnmatias@hotmail.com>
2022-04-28 16:19:34 -0300

    299. Used ViewSet in DRF to GET, POST, PATCH, PUT and DELETE

--- recipes/urls.py
+++ recipes/urls.py
@@ -48,13 +48,20 @@ urlpatterns = [
     ),
     path(
         'recipes/api/v2/',
-        views.RecipeAPIv2List.as_view(),
+        views.RecipeAPIv2ViewSet.as_view({
+            'get': 'list',
+            'post': 'create',
+        }),
         name='api_v2',
 
     ),
     path(
         'recipes/api/v2/<int:pk>/',
-        views.RecipeAPIv2Detail.as_view(),
+        views.RecipeAPIv2ViewSet.as_view({
+            'get': 'retrieve',
+            'patch': 'partial_update',
+            'delete': 'destroy'
+        }),
         name='api_v2_detail',
 
     ),
